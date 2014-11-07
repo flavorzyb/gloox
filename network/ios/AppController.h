8bbf021d7c6c8d194d4a32641da292e1ac5edfd0
flavorzhu <haker-haker@163.com>
2014-11-07 19:36:51 +0800

    add ios project

---
+++ network/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  networkAppController.h
+//  network
+//
+//  Created by admin on 14-11-7.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ network/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  networkAppController.h
+//  network
+//
+//  Created by admin on 14-11-7.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
