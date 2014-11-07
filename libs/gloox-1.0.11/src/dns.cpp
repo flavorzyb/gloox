896ab48d097f205d25847b016513562938997687
flavorzhu <haker-haker@163.com>
2014-11-07 17:34:24 +0800

    修正setsockopt 设置时间的bug

--- libs/gloox-1.0.11/src/dns.cpp
+++ libs/gloox-1.0.11/src/dns.cpp
@@ -403,22 +403,37 @@ namespace gloox
     }
 
 #ifdef HAVE_SETSOCKOPT
-      int nNetTimeout=1000;//1秒
-      //发送时限
-      setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, (char *)nNetTimeout, sizeof(int));
-      //接收时限
-      setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (char *)nNetTimeout, sizeof(int));
       
-      setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char*)&nNetTimeout, sizeof(int));
+    #if defined( _WIN32 )
+      int timeout = 3000;
+      
+      //发送时限
+      setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO,  &timeout, sizeof(int));
+      //接收时限
+      setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO,  (&timeout, sizeof(int));
+      
+      setsockopt(fd, SOL_SOCKET, SO_REUSEADDR,&timeout, sizeof(int));
+    #else
+      struct timeval timeout;
+      timeout.tv_sec = 3;
+      timeout.tv_usec = 0;
+      
+      //发送时限
+      setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO,  &timeout, sizeof(int));
+      //接收时限
+      setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO,  &timeout, sizeof(int));
+      
+      setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &timeout, sizeof(int));
+    #endif
       
       int keepAlive = 1;
       int keep_idle = 2;//开始首次KeepAlive探测前的TCP空闭时间
       int keep_interval = 1;//两次KeepAlive探测间的时间间隔
       int keep_count = 3;//判定断开前的KeepAlive探测次数
-      setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, (char*)&keepAlive, sizeof(keepAlive));
-      setsockopt(fd, SOL_TCP, TCP_KEEPIDLE, (void *)&keep_idle,sizeof(keep_idle));
-      setsockopt(fd, SOL_TCP,TCP_KEEPINTVL,(void *)&keep_interval, sizeof(keep_interval));
-      setsockopt(fd, SOL_TCP,TCP_KEEPCNT,(void *)&keep_count,sizeof(keep_count));
+      setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE,  &keepAlive,     sizeof(keepAlive));
+      setsockopt(fd, SOL_TCP, TCP_KEEPIDLE,     &keep_idle,     sizeof(keep_idle));
+      setsockopt(fd, SOL_TCP,TCP_KEEPINTVL,     &keep_interval, sizeof(keep_interval));
+      setsockopt(fd, SOL_TCP,TCP_KEEPCNT,       &keep_count,    sizeof(keep_count));
 #endif
 
     return (int)fd;
