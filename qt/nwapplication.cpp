90afa2e12a1cefd8873f918d60a4e8c2210824da
flavorzhu <haker-haker@163.com>
2014-11-10 13:02:21 +0800

    add connect debug info

--- qt/nwapplication.cpp
+++ qt/nwapplication.cpp
@@ -1,5 +1,7 @@
 #include <QtCore>
+#include <string>
 #include "nwapplication.h"
+#include "net/client.h"
 
 NWApplication::NWApplication(int &argc, char **argv):
     QCoreApplication(argc, argv)
@@ -13,11 +15,26 @@ NWApplication::~NWApplication()
 
 void NWApplication::run()
 {
+    connect(&m_thread, SIGNAL(finished()), this, SLOT(quit()));
     m_thread.start();
 }
 
 
 void NWThread::run()
 {
-    qDebug()<<"run........";
+    Client client;
+    qDebug()<<"will connect server ... ...";
+    if (!client.connect("zhuyanbin.com", 1111))
+    {
+        qDebug()<<"connect server fail.";
+    }
+
+    client.receiveWithPthread();
+
+    std::string data = "i am ok";
+    while(client.isConnect())
+    {
+        client.send(data.c_str(), data.length());
+        sleep(1);
+    }
 }
